ff01afebd83f177af1bebb269d63bf8f24ab5075
songshuyi <305679547@qq.com>
2016-08-07 15:59:01 +0800

    音乐播放器

---
+++ music/js/data.js
@@ -0,0 +1,7 @@
+var list = [
+    {name:"歌词里的那个谁",singer:"周小明",src:"audio/1.mp3"},
+    {name:"勇敢爱",singer:"Mi2",src:"audio/2.mp3"},
+    {name:"老街",singer:"李荣浩",src:"audio/3.mp3"},
+    {name:"走在冷风中",singer:"刘思涵",src:"audio/4.mp3"},
+    {name:"我好像在哪见过你",singer:"薛之谦",src:"audio/5.mp3"}
+];
